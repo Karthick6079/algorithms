01303242ed70237137b59ca7becc837e7d7f9634
karth <karthick.vasudevan07@gmail.com>
2023-09-16 13:48:36 +0530

    Added Junit 5.8.1 dependencies in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     implementation("junit:junit:4.13.1")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter",  "junit-jupiter-engine",  "5.9.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
 }
 
 tasks.test {
